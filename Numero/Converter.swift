f7b88a609c8b44e051402704c6b562058464ac64
Stanley Ejechi <sejechi@gmail.com>
2019-07-02 15:46:06 -0400

    Added travis.yml

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -26,7 +26,7 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-class Converter {
+class ConverterTests {
   func convert(_ number: Int) -> String {
     return ""
   }
